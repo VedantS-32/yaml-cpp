712b1f5ff3f281bfc6f0efc517e0b6ac10e659ff
VedantS <122535961+VedantS-32@users.noreply.github.com>
2023-11-27 15:34:47 +0530

    Created premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,22 @@
+project "yaml-cpp"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++20"
+	staticruntime "Off"
+	systemversion "latest"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"src/**.h",
+		"src/**.cpp",
+		
+		"include/**.h"
+	}
+
+	includedirs
+	{
+		"include"
+	}
